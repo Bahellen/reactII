b4b830007cc914d6527c504a04cfe42a8d16725e
Bahellen <barbarahellen2909@gmail.com>
2022-07-19 14:57:46 -0300

    aula-reactII

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./pages/Header/Header"
+import Sobre from "./pages/Sobre/SectionApr.jsx"
+import Projetos from "./pages/Projetos/Listaprojetos"
+import Repositorios from "./pages/Repositorios/Repositorios"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Sobre />
+      <Projetos />
+      <Repositorios />
+      
+    </>
+  
+  )
+}
+
+export default App
---
+++ src/pages/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+function Header() {
+
+
+    return (
+        <div className='header'>
+
+            <h1>Meu Portfolio Reprograma</h1>
+         
+        </div>
+    )
+  }
+  
+  export default Header
